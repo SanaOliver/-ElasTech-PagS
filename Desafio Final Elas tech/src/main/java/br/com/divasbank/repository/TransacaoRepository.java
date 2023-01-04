15ad6f177d1146eaec627914e414231f688c7a38
ROSANA OLIVEIRA <115505644+SanaOliver@users.noreply.github.com>
2023-01-04 20:01:53 -0300

    delete

--- Desafio Final Elas tech/src/main/java/br/com/divasbank/repository/TransacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.divasbank.repository;
-
-import br.com.divasbank.model.Transacao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransacaoRepository extends JpaRepository<Transacao, Long> {
-}
